76a6ab2f46ae9698eb8251a1b91a07f14137800f
ngoctan392k4 <ngoctan300904@gmail.com>
2025-11-17 18:58:39 +0700

    Initialize database config

---
+++ src/utils/pgConfig.mjs
@@ -0,0 +1,11 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+  user: process.env.db_user,
+  host: process.env.db_host,
+  database: process.env.db_name,
+  password: process.env.db_password,
+  port: process.env.db_port,
+});
+
+export default pool;
