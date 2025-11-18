0ad5ea678e936860e1b0cd2ac4249bd34f5da9bf
kienngo <ngothanhkien2004@gmail.com>
2025-11-18 22:42:09 +0700

    pulling

--- src/index.mjs
+++ src/index.mjs
@@ -1,21 +1,35 @@
-import express from "express"
-import cors from 'cors'
+import express, { response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import pool from "./utils/pgConfig.mjs";
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
-})
+  console.log(`Server is running on ${PORT}`);
+});
 
 app.get("/", (request, response) => {
-    return response.send({msg: "Hello"})
-})
+  return response.send({ msg: "Hello" });
+});
+
+app.get("/api/admin/CourseManagement", async (request, response) => {
+  try {
+    const result = await pool.query("SELECT * FROM courses");
+    return response.json(result.rows);
+  } catch (error) {
+    console.error("DB ERROR:", error.message);
+    return response.status(500).send("Database Error");
+  }
+});
