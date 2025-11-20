23251bd4996b8a7a81e7e1d6fcdfaa257e48430e
Pen <pennguyen31@gmail.com>
2025-11-20 16:53:40 +0700

    Initialize backend logic to the View Account Function

--- src/routes/routes.mjs
+++ src/routes/routes.mjs
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from '../auth/auth.mjs'
 import viewCourseRouter from '../admin/CourseManagement/ViewCourse.mjs'
+import viewAccountRouter from '../admin/AccountManagement/viewAccountList.mjs'
 
 const router = Router();
 
 router.use(authRouter);
 router.use(viewCourseRouter);
+router.use(viewAccountRouter);
 
 export default router
