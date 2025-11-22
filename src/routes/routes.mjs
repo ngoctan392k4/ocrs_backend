50a69d6bd67eeecd5a010bd107ad9cef0cc022e7
Thi <mthi12347@gmail.com>
2025-11-22 19:18:50 +0700

    US24AddClass

--- src/routes/routes.mjs
+++ src/routes/routes.mjs
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import authRouter from '../auth/auth.mjs';
 import classRoute from '../admin/ClassManagement/ViewClass.mjs';
+import addclassRoute from '../admin/ClassManagement/AddClass.mjs'
+import viewCourseRouter from '../admin/CourseManagement/ViewCourse.mjs'
 
 const router = Router();
 
 router.use(authRouter);
 router.use(classRoute);
+router.use(addclassRoute);
 router.use(viewCourseRouter);
 
 export default router
