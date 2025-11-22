72bfd5166aa0fa20eea09c6701759b82454d0ac1
Thi <mthi12347@gmail.com>
2025-11-23 02:40:09 +0700

    US27_2311

--- src/admin/ClassManagement/ViewClass.mjs
+++ src/admin/ClassManagement/ViewClass.mjs
@@ -5,15 +5,25 @@ const router = Router();
 
 router.get("/api/admin/ClassManagement", async (req, res) => {
   try {
-    const classesResult = await pool.query("SELECT * FROM getClassesWithSchedule()");
+
+  const classesResult = await pool.query("SELECT * FROM getClassesWithSchedule()");
+    // Lấy course
+    const courseResult = await pool.query("SELECT * FROM get_course()");
+
+    // Lấy instructor
+    const instructorResult = await pool.query("SELECT * FROM get_allinstructor()");
+    const Semester = await pool.query("SELECT * FROM get_semester()");
 
     return res.json({
       classes: classesResult.rows, 
+      courses: courseResult.rows,
+      instructors: instructorResult.rows,
+      semester: Semester.rows
     });
 
   } catch (error) {
     console.error("DB ERROR:", error.message);
-    return res.status(500).json({ error: "Database Error" });
+    return res.status(500).json({ message: "Database Error" });
   }
 });
 
