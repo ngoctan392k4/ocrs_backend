ebf89bff3fa8753db850433e296bfb8c41eeb038
NGUYEN QUANG THINH <109730073+Pennguyen1012@users.noreply.github.com>
2025-11-22 20:53:45 +0700

    Remove editAccountRouter from routes

--- src/routes/routes.mjs
+++ src/routes/routes.mjs
@@ -6,7 +6,6 @@ import viewCourseRouter from '../admin/CourseManagement/ViewCourse.mjs'
 
 import viewAccountRouter from '../admin/AccountManagement/viewAccountList.mjs'
 import addAccountRouter from '../admin/AccountManagement/AddAccount.mjs'
-import editAccountRouter from '../admin/AccountManagement/EditAccount.mjs'
 import DeleteAccountRouter from '../admin/AccountManagement/DeleteAccount.mjs'
 
 const router = Router();
@@ -18,7 +17,6 @@ router.use(courseDeleteRoute);
 
 router.use(viewAccountRouter);
 router.use(addAccountRouter);
-router.use(editAccountRouter);
 router.use(DeleteAccountRouter);
 
 
