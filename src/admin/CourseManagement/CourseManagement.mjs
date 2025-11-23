389db3b41aef61ba9380f6e0a2d717dd87fb8e7d
kienngo <ngothanhkien2004@gmail.com>
2025-11-23 22:04:56 +0700

    Close to final ver

--- src/admin/CourseManagement/CourseManagement.mjs
+++ src/admin/CourseManagement/CourseManagement.mjs
@@ -36,17 +36,37 @@ router.post("/api/admin/CourseManagement", async (request, response) => {
         message: "At least one credit must be greater than 0",
       });
     }
-    const { rows: existsRows } = await pool.query(
+    const { rows: existsRows_id } = await pool.query(
       `SELECT checkID_existed($1) AS exists`,
       [courseID]
     );
 
-    if (existsRows[0].exists) {
+    if (courseID == courseCode) {
+      return response.status(400).json({
+        success: false,
+        message: "Invalid CourseID -- missing code number",
+      });
+    }
+
+    if (existsRows_id[0].exists) {
       return response.status(400).json({
         success: false,
         message: `Course ID ${courseID} already exists. Please use a different Course ID.`,
       });
     }
+
+    const { rows: existsRows_name } = await pool.query(
+      `SELECT checkCourseNameExists($1) AS exists`,
+      [courseName]
+    );
+
+    if (existsRows_name[0].exists) {
+      return response.status(400).json({
+        success: false,
+        message: `Course name "${courseName}" already exists. Please use a different Course Name.`,
+      });
+    }
+
     await pool.query(
       `CALL insert_course(
         $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19
@@ -56,9 +76,9 @@ router.post("/api/admin/CourseManagement", async (request, response) => {
         courseID,
         courseName,
         tosu,
-        pre || null,
-        para || null,
-        des || null,
+        pre || "No prerequisite",
+        para || "No parallel courses",
+        des || "No description",
         credits.LEC,
         credits.LAB,
         credits.Review,
@@ -121,6 +141,7 @@ router.get(
       if (result.rows.length === 0) {
         return response.status(404).json({ message: "Course not found" });
       }
+
       const row = result.rows[0];
 
       return response.json({
@@ -162,6 +183,21 @@ router.put(
           message: "CourseID and CourseName are required",
         });
       }
+
+      // tam thoi ko check trung cho Edit
+
+      // const { rows: existsRows_name } = await pool.query(
+      //   `SELECT checkCourseNameExists($1) AS exists`,
+      //   [courseName]
+      // );
+
+      // if (existsRows_name[0].exists) {
+      //   return response.status(400).json({
+      //     success: false,
+      //     message: `Course name "${courseName}" already exists. Please use a different Course Name.`,
+      //   });
+      // }
+
       const allCreditsZero = Object.values(credits || {}).every((c) => c === 0);
       if (allCreditsZero) {
         return response.status(400).json({
