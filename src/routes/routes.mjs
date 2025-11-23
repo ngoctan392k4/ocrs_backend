cb541b849da9e3733f3b1fc6f2b9707f1274f7e3
ngoctan392k4 <115345108+ngoctan392k4@users.noreply.github.com>
2025-11-23 23:40:04 +0700

    Clean up routes by removing unused imports
    Removed unused routes for adding classes and editing classes.

--- src/routes/routes.mjs
+++ src/routes/routes.mjs
@@ -1,12 +1,9 @@
 import { Router } from "express";
 import authRouter from '../auth/auth.mjs';
 import classRoute from '../admin/ClassManagement/ViewClass.mjs';
-import addclassRoute from '../admin/ClassManagement/AddClass.mjs'
 import courseDeleteRoute from '../admin/CourseManagement/DeleteCourse.mjs';
 import viewCourseRouter from '../admin/CourseManagement/ViewCourse.mjs'
-import viewAccountRouter from '../admin/AccountManagement/viewAccountList.mjs'
-import editClassRouter from '../admin/ClassManagement/AddClass.mjs'
-import courseDeleteRoute from '../admin/CourseManagement/DeleteCourse.mjs';
+import viewAccountRouter from '../admin/AccountManagement/viewAccountList.mjs' 
 import addAccountRouter from '../admin/AccountManagement/AddAccount.mjs'
 import DeleteAccountRouter from '../admin/AccountManagement/DeleteAccount.mjs'
 
@@ -14,12 +11,10 @@ const router = Router();
 
 router.use(authRouter);
 router.use(classRoute);
-router.use(addclassRoute);
 router.use(viewCourseRouter);
 router.use(courseDeleteRoute);
 router.use(viewAccountRouter);
 router.use(addAccountRouter);
-router.use(editClassRouter);
 router.use(DeleteAccountRouter);
 
 export default router
