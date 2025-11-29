d53c95ef49f85165dc235252dd5b34fa0483fec5
kienngo <ngothanhkien2004@gmail.com>
2025-11-29 22:48:33 +0700

    style(OpenCourse): Update showing sem name in the OpenCourse interface

--- src/admin/OpenCourse/OpenCourse.mjs
+++ src/admin/OpenCourse/OpenCourse.mjs
@@ -7,45 +7,51 @@ const router = Router();
 
 router.get("/api/admin/openCourse", async (request, response) => {
   try {
-    const result = await pool.query("SELECT * FROM get_courses_not_in_upcoming()");
-
-    return response.json(
-    result.rows
+    const result = await pool.query(
+      "SELECT * FROM get_courses_not_in_upcoming()"
     );
+    const latestSemester = await pool.query(
+      "SELECT * FROM get_latest_semester()"
+    );
+
+    return response.json({
+      course: result.rows,
+      semester: latestSemester.rows,
+    });
   } catch (error) {
     console.error("DB ERROR:", error.message);
     return response.status(500).send("Database Error");
   }
 });
 
-router.post("/api/admin/openCourse", async (req, res) => {
-  const { courses } = req.body; 
+router.post("/api/admin/openCourse", async (request, response) => {
+  const { courses } = request.body;
 
   if (!courses || courses.length == 0) {
-    return res.status(400).json({ message: "No courses selected" });
+    return response.status(400).json({ message: "No courses selected" });
   }
 
   let count = 0;
   try {
     for (const courseid of courses) {
       await pool.query("CALL add_upcoming_course($1)", [courseid]);
-      count+=1;
+      count += 1;
     }
 
-    return res.json({ message: `${count} course(s) opened successfully` });
+    return response.json({ message: `${count} course(s) opened successfully` });
   } catch (error) {
     console.error("DB ERROR:", error.message);
-    return res.status(500).json({ error: "Failed to open courses" });
+    return response.status(500).json({ error: "Failed to open courses" });
   }
 });
 
-router.post("/api/admin/semester/next", async (req, res) => {
+router.post("/api/admin/semester/next", async (request, response) => {
   try {
     await pool.query("CALL create_next_semester()");
-    return res.json({ message: "Semester incremented" });
+    return response.json({ message: "Semester incremented" });
   } catch (err) {
     console.error("SEMESTER ERROR:", err.message);
-    return res.status(500).json({ error: "Database Error" });
+    return response.status(500).json({ error: "Database Error" });
   }
 });
 
