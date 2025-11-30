811efa80cb8a1e8edef74e42a79770d3c76ba9d4
kienngo <ngothanhkien2004@gmail.com>
2025-11-30 18:38:56 +0700

    feat(OpenCourse): Implement cannot remove course that has student enrolled

--- src/admin/OpenCourse/OpenCourse.mjs
+++ src/admin/OpenCourse/OpenCourse.mjs
@@ -7,18 +7,17 @@ const router = Router();
 
 router.get("/api/admin/openCourse", async (request, response) => {
   try {
-    const latestSemester = await pool.query("SELECT * FROM get_latest_semester()");
+    const latestSemester = await pool.query(
+      "SELECT * FROM get_latest_semester()"
+    );
     const latestSem = latestSemester.rows[0];
     const semid = request.query.semid || latestSem?.semid;
 
-    const course = await pool.query(
-      "SELECT * FROM get_courses_by_semid($1)",
-      [semid]
-    );
+    const course = await pool.query("SELECT * FROM get_courses_by_semid($1)", [
+      semid,
+    ]);
 
-    const courses = await pool.query(
-      "SELECT * FROM get_course()"
-    )
+    const courses = await pool.query("SELECT * FROM get_course()");
 
     const allSem = await pool.query("SELECT * FROM get_all_semesters()");
 
@@ -26,9 +25,8 @@ router.get("/api/admin/openCourse", async (request, response) => {
       allCourse: courses.rows,
       course: course.rows,
       latestSem,
-      allSem: allSem.rows
+      allSem: allSem.rows,
     });
-
   } catch (error) {
     console.error("DB ERROR:", error.message);
     return response.status(500).send("Database Error");
@@ -39,12 +37,13 @@ router.get("/api/admin/openCourse", async (request, response) => {
 router.put("/api/admin/openCourse", async (request, response) => {
   const { add, remove } = request.body;
 
-  if (!add && !remove || add.length == 0 && remove.length == 0) {
+  if ((!add && !remove) || (add.length == 0 && remove.length == 0)) {
     return response.status(400).json({ message: "No update" });
   }
 
   let addCount = 0;
   let removeCount = 0;
+  let courseHasStudent = [];
   try {
     for (const courseid of add) {
       await pool.query("CALL add_upcoming_course($1)", [courseid]);
@@ -52,15 +51,36 @@ router.put("/api/admin/openCourse", async (request, response) => {
     }
 
     for (const courseid of remove) {
-      await pool.query("CALL remove_upcoming_course($1)", [courseid]);
-      removeCount += 1;
+      const result = await pool.query(
+        "SELECT checkCourseHasEnrolledStudents($1) AS has_student",
+        [courseid]
+      );
+
+      const hasStudent = result.rows[0].has_student;
+
+      if (hasStudent) {
+        courseHasStudent.push(courseid);
+      } else {
+        await pool.query("CALL remove_upcoming_course($1)", [courseid]);
+        removeCount += 1;
+      }
     }
 
-    const addMsg = addCount != 0 ? `${addCount} course(s) opened` : "No new course opened"
-    const removeMsg = removeCount != 0 ? `${removeCount} course(s) removed` : "No course removed"
+    let cannotRemoveMsg = "";
+    if (courseHasStudent.length > 0) {
+      cannotRemoveMsg = ` ${courseHasStudent.join(", ")} cannot be remove`;
+    }
 
+    const addMsg =
+      addCount != 0 ? `${addCount} course(s) opened` : "No new course opened";
+    const removeMsg =
+      removeCount != 0
+        ? `${removeCount} course(s) removed`
+        : "No course removed";
 
-    return response.json({ message: addMsg +", "+ removeMsg });
+    const msg = cannotRemoveMsg == "" ?  addMsg + ", " + removeMsg : addMsg + ", " + removeMsg + ", "+ cannotRemoveMsg;
+    return response.json({ message: msg });
+    
   } catch (error) {
     console.error("DB ERROR:", error.message);
     return response.status(500).json({ error: "Failed to open courses" });
