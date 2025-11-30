757f5f05141ed33c2649bcc585bb448438a2b7cd
Thi <mthi12347@gmail.com>
2025-11-30 19:27:30 +0700

    Update View-Delete Registered Classlan 3

--- src/student/ClassRegistration/RegisteredClass.mjs
+++ src/student/ClassRegistration/RegisteredClass.mjs
@@ -6,12 +6,22 @@ const router = Router();
 router.get("/api/student/registeredClass", async (req, res) => {
   try {
     const accountid = req.user.accountid;
-    const latestSemester = await pool.query("SELECT * FROM get_latest_semester()");
-    const viewRegisteredResult = await pool.query("SELECT * FROM getInitialRegistrations($1)", [accountid]);
+
+    const currentSemResult = await pool.query("SELECT * FROM get_current_semester()");
+    const currentSem = currentSemResult.rows[0];
+
+    const latestSemResult = await pool.query("SELECT * FROM get_latest_semester()");
+    const latestSem = latestSemResult.rows[0];
+
+    const viewRegisteredResult = await pool.query(
+      "SELECT * FROM getInitialRegistrations($1)",
+      [accountid]
+    );
 
     return res.json({
       registered: viewRegisteredResult.rows,
-      semesterlat: latestSemester.rows
+      currentSem, 
+      latestSem   
     });
 
   } catch (error) {
@@ -21,16 +31,15 @@ router.get("/api/student/registeredClass", async (req, res) => {
 });
 
 router.delete("/api/student/registeredClass/:clsid", async (req, res) => {
-    const accountid = req.user.accountid;
-    const { clsid } = req.params;  
-    try {
-        await pool.query("CALL delete_registeredclass($1, $2)", [accountid, clsid]);
-
-        return res.json({ message: `Class ${clsid} deleted successfully.` });
-    } catch (err) {
-        console.error("DELETE CLASS ERROR:", err.message);
-        return res.status(500).json({ error: "Database Error" });
-    }
+  const accountid = req.user.accountid;
+  const { clsid } = req.params;  
+  try {
+    await pool.query("CALL delete_registeredclass($1, $2)", [accountid, clsid]);
+    return res.json({ message: `Class ${clsid} deleted successfully.` });
+  } catch (err) {
+    console.error("DELETE CLASS ERROR:", err.message);
+    return res.status(500).json({ error: "Database Error" });
+  }
 });
 
 export default router;
